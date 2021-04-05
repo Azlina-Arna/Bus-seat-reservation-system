66839d5d724e08f8a7c7cb35bb58db7b8af30ca0
Azlina <azlinaaurna@gmail.com>
2021-04-05 16:42:09 +0800

    Azlina update

---
+++ booking.php
@@ -0,0 +1,87 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'reservation');
+if(isset($_GET['seat'])){
+    $seat = $_GET['seat'];
+}
+
+
+if(isset($_POST['submit'])){
+  $fname = $_POST['fname'];
+  $email = $_POST['email'];
+  $icnumber = $_POST['icnumber'];
+  $phonenumber=$_POST['phonenumber'];
+  $stmt = $mysqli->prepare("select * from reserve where seat = ?");
+    $stmt->bind_param('s', $seat);
+    $reserve = array();
+    if($stmt->execute()){
+        $result = $stmt->get_result();
+        if($result->num_rows>0){
+            $msg = "<div>This seat has been already Booked.</div>";
+        }else{
+  $stmt= $mysqli->prepare("INSERT INTO reserve (fname, email, icnumber, phonenumber,seat) VALUES(?,?,?,?,?)");
+  $stmt->bind_param('sssis', $fname, $email, $icnumber, $phonenumber, $seat);
+  $stmt->execute();
+  $msg = "<div>You Successfully reserved your seat</div>";
+  $stmt->close();
+  $mysqli->close();
+}
+    }
+
+  }
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>bus seat reservation</title>
+    <link rel="stylesheet" type="text/css" href="booking.css">
+    
+</head>
+<header>
+
+    <div class="menu">
+        <img src="main.jpg" style="width:100%" height="550px">
+
+        <nav>
+            <ul>
+            <li><a href="index.php"><strong>Home</strong></a></li>
+            <li><a href="about.html"><strong>About</strong></a></li>
+            <li><a href="history.html"><strong>Booking History</strong></a></li>
+         
+            </ul>
+        </nav>
+    </div>
+
+</header>
+<body>
+<div class="box">
+<div class="form">
+<?php echo(isset($msg))?$msg:""; ?>
+<div class="container">
+  <form action=" " method="post" autocomplete="off">
+    <h3>Reservation Form</h3>
+    <h2>Reservation for seat number: <?php echo $seat ?> </h2>
+    <label for="name">First Name</label>
+    <input type="text" id="fname" name="fname" placeholder="Your name..">
+
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" placeholder="Your email..">
+
+    <label for="ic/passport">IC/Passport Number</label>
+    <input type="text" id="icnumber" name="icnumber" placeholder="Your IC/passport number..">
+
+    <label for="phone">Phone Number</label>
+    <input type="text" id="phonenumber" name="phonenumber" placeholder="Your phone number..">
+
+    <input type="submit", name="submit" value="Submit">
+  </form>
+</div>
+</div>
+</div>
+</body>
+</html>
+            
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'reservation');
+$stmt= $mysqli->prepare("select * from reserve where seat=?");
+$stmt-> bind_param('s', $seat);
+$reserve= array();
+if($stmt->execute()){
+$result= $stmt->get_result();
+if($result->num_rows>0){
+    while($row = $result->fetch_assoc()){
+        $reserve [] = $row['seat'];
+    }
+$stmt->close();
+}
+}
+
+$seats= [
+    ['window'=> '1A', 'double'=> '1B',  'Single'=> '1C'],
+    ['window'=> '2A', 'double'=> '2B',  'Single'=> '2C'],
+    ['window'=> '3A', 'double'=> '3B',  'Single'=> '3C'],
+    ['window'=> '4A', 'double'=> '4B',  'Single'=> '4C'],
+    ['window'=> '5A', 'double'=> '5B',  'Single'=> '5C'],
+    ['window'=> '6A', 'double'=> '6B',  'Single'=> '6C'],
+    ['window'=> '7A', 'double'=> '7B',  'Single'=> '7C'],
+    ['window'=> '8A', 'double'=> '8B',  'Single'=> '8C'],
+    ['window'=> '9A', 'double'=> '9B',  'Single'=> '9C'],
+    ['window'=> '10A', 'double'=> '10B',  'Single'=> '10C']
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>bus seat reservation</title>
+    <link rel="stylesheet" type="text/css" href="home.css">
+    
+</head>
+<header>
+
+    <div class="menu">
+        <img src="main.jpg" style="width:100%" height="550px">
+
+        <nav>
+            <ul>
+            <li><a href="index.php"><strong>Home</strong></a></li>
+            <li><a href="about.html"><strong>About</strong></a></li>
+            <li><a href="history.html"><strong>Booking History</strong></a></li>
+         
+            </ul>
+        </nav>
+    </div>
+
+</header>
+
+<body>
+<div class= "seat" >
+    <h2>Choose your Seat for reservation</h2> 
+    <table class= "center">
+        <tr>
+            <th>Window seat</th>
+            <th>Double seat</th>
+            <th>Single seat</th>
+        </tr>
+
+        <?php
+        foreach ($seats as $s){?>
+
+        <tr>
+            <td><?php echo $s['window'];?> 
+           <?php if(in_array($seat, $reserve)){ ?>
+                <a href='index.php' class= "btn">Booked</a>
+            <?php } else{ ?>
+            <a href='booking.php?seat=<?php echo $s['window'];?>' class= "btn">Reserve</a> 
+            <?php } ?> 
+        </td>
+            <td><?php echo $s['double'];?>
+            <a href='booking.php?seat=<?php echo $s['double'];?>' class= "btn">Reserve</a> </td>
+            <td><?php echo $s['Single'];?>
+            <a href='booking.php?seat=<?php echo $s['Single'];?>' class= "btn">Reserve</a> </td>
+        </tr>
+
+       <?php } ?>
+    </table>
+</div>
+</body>
+</html>
